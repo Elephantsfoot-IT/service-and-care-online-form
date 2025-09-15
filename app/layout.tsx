849bb7a9c7cff19cecdaa992bd0a39556bad6528
Elephantsfoot-IT <steven.trinh@elephantsfoot.com.au>
2025-09-15 12:46:17 +1000

    add roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { roboto } from "@/lib/font";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "EFG Service and Care",
@@ -23,9 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.variable}>
+      <head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/ef-service-care-logomark.svg"
+        />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
---
+++ lib/font.ts
@@ -0,0 +1,9 @@
+// app/fonts.ts
+import { Roboto} from 'next/font/google'
+
+export const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'], // Include all needed weights
+  variable: '--font-roboto',
+  display: 'swap',
+})
