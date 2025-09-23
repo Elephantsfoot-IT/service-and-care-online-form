cbc1bb870b62c964a48dc5cc6f25a5d4880be9c4
Elephantsfoot-IT <steven.trinh@elephantsfoot.com.au>
2025-09-23 21:13:04 +1000

    animated not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,26 @@
-import { XCircleIcon } from "lucide-react";
+import { CircleXIcon } from "@/components/animate-ui/icons/circle-x";
 import React from "react";
 
 function NotFound() {
   return (
     <div className="pt-[88px]">
-      <div className="min-h-[calc(100vh-88px)] flex items-center justify-center px-4 translate-y-[-100px] fade-in">
+      <div className="min-h-[calc(100vh-88px)] flex items-center justify-center px-4 translate-y-[-100px] fade-up">
         <div className="w-full max-w-xl bg-white p-8 text-center">
           <div className="mx-auto mb-4 flex items-center justify-center">
-            <XCircleIcon
+            <CircleXIcon
+              animateOnView
+              animation="default"
               className="h-20 w-20 text-red-500"
               aria-hidden="true"
             />
           </div>
 
           <h1 className="text-2xl font-semibold text-neutral-900">
-            Service Agreement Not Found
+             Form Not Found
           </h1>
 
           <p className="mt-3 text-base text-neutral-600">
-            This service agreement link is invalid, expired, or may have already
+            This form is invalid, expired, or may have already
             been completed.
           </p>
         </div>
--- app/service-agreement/success/page.tsx
+++ app/service-agreement/success/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { CircleCheck } from "@/components/animate-ui/icons/circle-check";
-import Header from "@/components/header";
 
 export default function Success() {
   return (
     <>
 
       <div className="pt-[88px]">
-        <div className="min-h-[calc(100vh-88px)] flex items-center justify-center px-4 translate-y-[-100px] fade-in">
+        <div className="min-h-[calc(100vh-88px)] flex items-center justify-center px-4 translate-y-[-100px] fade-up">
           <div className="w-full max-w-xl  bg-white p-8 text-center">
             <div className="mx-auto mb-4 flex items-center justify-center">
               <CircleCheck animateOnView animation="default" className="h-20 w-20 text-efg-yellow" />
