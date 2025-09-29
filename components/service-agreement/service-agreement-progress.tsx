d01be31a23549e141515e1d2a7c4299b88de2383
Elephantsfoot-IT <steven.trinh@elephantsfoot.com.au>
2025-09-29 11:36:20 +1000

    mobile form step

--- components/service-agreement/service-agreement-progress.tsx
+++ components/service-agreement/service-agreement-progress.tsx
@@ -3,24 +3,13 @@ import { Progress } from "@/components/ui/progress";
 
 function ServiceAgreementProgress() {
   const state = useServiceAgreementStore();
-  const value = (state.page ?? 0)/3 * 100;
+  const value = ((state.page ?? 0) / 4) * 100;
   if (state.page === 1) return null;
   return (
     <>
       <div className="w-full flex flex-col gap-2 mr-auto xl:hidden">
-       {/* <div className="text-sm text-neutral-800 font-medium">{state.page}/7</div> */}
-       <div className="flex flex-row items-center justify-between">
-       <div className="text-sm font-medium">
-        Step {state.page} of 3
-       </div>
-       <div className="text-sm text-neutral-600">
-        {state.page === 1 && "Services Details"}
-        {state.page === 2 && "Customer Information"}
-        {state.page === 3 && "Review & Sign Agreement"}
-       </div>
-       </div>
-       
-       <Progress value={value} className="bg-neutral-200" />
+        <Progress value={value} className="bg-neutral-200" />
+        
       </div>
     </>
   );
