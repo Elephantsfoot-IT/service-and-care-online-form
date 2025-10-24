b344e667ea9ef1a4519d2fbb71dbed2850df1808
Elephantsfoot-IT <steven.trinh@elephantsfoot.com.au>
2025-10-24 11:05:53 +1100

    redesign chute cleaning

--- components/service-agreement/services/chute-cleaning-section.tsx
+++ components/service-agreement/services/chute-cleaning-section.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import { SectionShell, SectionHeader, SectionDetails, SectionContent, ChuteFooter } from "./service-helper";
+import {
+  SectionShell,
+  SectionHeader,
+  SectionDetails,
+  SectionContent,
+  ChuteFooter,
+} from "./service-helper";
 import ServiceFrequency2 from "@/components/service-agreement/service-frequency-2";
 import { formatMoney, getNumber } from "@/lib/utils";
 import { GetServicesReturnTyped, MaybeOption, options } from "@/lib/interface";
 
 type Props = {
-  details:  GetServicesReturnTyped<"chute_cleaning"> ;
+  details: GetServicesReturnTyped<"chute_cleaning">;
   frequency: MaybeOption | null;
   onChangeFrequency: (v: MaybeOption) => void;
   discount: number;
@@ -37,7 +43,11 @@ export default function ChuteCleaningSection({
           <li>Use odour-control spray to neutralize smells...</li>
           <li>
             For more information, please visit{" "}
-            <a href="https://www.elephantsfoot.com.au/chute-cleaning/" className="underline text-efg-yellow" target="_blank">
+            <a
+              href="https://www.elephantsfoot.com.au/chute-cleaning/"
+              className="underline text-efg-yellow"
+              target="_blank"
+            >
               our website
             </a>
           </li>
@@ -45,7 +55,11 @@ export default function ChuteCleaningSection({
       </SectionDetails>
 
       <SectionContent>
-        <ServiceFrequency2 value={frequency} onChange={onChangeFrequency} options={options} />
+        <ServiceFrequency2
+          value={frequency}
+          onChange={onChangeFrequency}
+          options={options}
+        />
 
         {/* Desktop */}
         <div className="hidden sm:block w-full bg-neutral-75 rounded-xl p-4">
@@ -54,51 +68,77 @@ export default function ChuteCleaningSection({
               <div className="col-span-3 px-2 py-2">Sites</div>
               <div className="col-span-1 px-2 py-2">Level</div>
               <div className="col-span-1 px-2 py-2">Chutes</div>
-              <div className="col-span-1 text-right px-2 py-2">Price per chute</div>
+              <div className="col-span-1 text-right px-2 py-2">
+                Price per chute <br></br> (excl. GST)
+              </div>
             </div>
 
             {details.items.map((r, i) => (
-              <div key={i} className="grid grid-cols-6 gap-2 border-b border-input last:border-b-0 text-sm xl:text-base">
+              <div
+                key={i}
+                className="grid grid-cols-6 gap-2 border-b border-input last:border-b-0 "
+              >
                 {r.building_name ? (
-                  <div className="col-span-3 px-2 py-2">
-                    <div className="font-medium">{r.site_name}</div>
-                    <div>{r.building_name}</div>
+                  <div className="col-span-3 px-2 py-2 flex flex-col gap-1">
+                    <div className="font-medium text-sm xl:text-base">
+                      {r.site_name}
+                    </div>
+                    <div className="text-xs xl:text-sm">{r.building_name}</div>
                   </div>
                 ) : (
-                  <div className="col-span-3 px-2 py-2 font-medium">{r.site_name}</div>
+                  <div className="col-span-3 px-2 py-2 font-medium">
+                    {r.site_name}
+                  </div>
                 )}
                 <div className="col-span-1 px-2 py-2">{r.levels}</div>
                 <div className="col-span-1 px-2 py-2">{r.chutes}</div>
-                <div className="col-span-1 text-right px-2 py-2">{formatMoney(getNumber(r.price))}</div>
+                <div className="col-span-1 text-right px-2 py-2">
+                  {formatMoney(getNumber(r.price))}
+                </div>
               </div>
             ))}
           </div>
         </div>
 
         {/* Mobile */}
-        <div className="sm:hidden w-full flex flex-col rounded-xl bg-neutral-75 p-4">
+        <div className="sm:hidden w-full flex flex-col rounded-xl bg-neutral-75 px-2 py-4">
           <div className="grid grid-cols-2 gap-2 border-b border-input text-sm xl:text-base">
             <div className="col-span-1 px-2 py-2 text-xs">Services</div>
-            <div className="col-span-1 text-right px-2 py-2 text-xs">Price</div>
+            <div className="col-span-1 text-right px-2 py-2 text-xs">
+              Price per chute <br></br> (excl. GST)
+            </div>
           </div>
           {details.items.map((r, i) => (
-            <div key={i} className="flex flex-row gap-2 border-b border-input last:border-b-0 text-sm xl:text-base">
-              <div className="w-full px-2 py-2">
-                <div className="flex flex-col gap-1">
-                  <div className="font-medium">{r.site_name}</div>
-                  {r.building_name && <div className="text-xs">{r.building_name}</div>}
-                  <div className="flex flex-row gap-2 items-center">
-                    <div className="text-xs">{r.levels} × Levels</div>
-                    <div className="text-xs">{r.chutes} × Chutes</div>
+            <div
+              key={i}
+              className="flex flex-col border-b border-input last:border-b-0 py-2 px-2"
+            >
+              <div className="font-medium text-sm">
+                {r.site_name} {r.building_name && `- ${r.building_name}`}
+              </div>
+              <div className="flex flex-row gap-2 items-center justify-between text-sm pl-1">
+          
+                  <div className="flex flex-col gap-1">
+                    <div className="flex flex-row gap-2 items-center">
+                      <div className="">{r.levels} × Levels</div>
+                      <div className="">{r.chutes} × Chutes</div>
+                    </div>
                   </div>
+                
+                <div className="text-right px-2 py-2 w-fit flex-shrink-0">
+                  {formatMoney(getNumber(r.price))}
                 </div>
               </div>
-              <div className="text-right px-2 py-2 w-fit flex-shrink-0">{formatMoney(getNumber(r.price))}</div>
             </div>
           ))}
         </div>
 
-        <ChuteFooter items={details.items} frequency={frequency} discountPct={discount} incentives={incentives} />
+        <ChuteFooter
+          items={details.items}
+          frequency={frequency}
+          discountPct={discount}
+          incentives={incentives}
+        />
       </SectionContent>
     </SectionShell>
   );
