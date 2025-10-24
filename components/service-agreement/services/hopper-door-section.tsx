aa6a12988b9b623801d4e0d91282f97d0eb181c8
Elephantsfoot-IT <steven.trinh@elephantsfoot.com.au>
2025-10-24 11:15:03 +1100

    hopper door section redesign

--- components/service-agreement/services/hopper-door-section.tsx
+++ components/service-agreement/services/hopper-door-section.tsx
@@ -54,42 +54,66 @@ export default function HopperDoorInspectionSection({
               <div className="col-span-3 px-2 py-2">Sites</div>
               <div className="col-span-1 px-2 py-2">Level</div>
               <div className="col-span-1 px-2 py-2">Chutes</div>
-              <div className="col-span-1 text-right px-2 py-2">Price per chute</div>
+              <div className="col-span-1 text-right px-2 py-2">
+                Price per chute <br></br> (excl. GST)
+              </div>
             </div>
 
             {details.items.map((r, i) => (
-              <div key={i} className="grid grid-cols-6 gap-2 border-b border-input last:border-b-0 text-sm xl:text-base">
+              <div
+                key={i}
+                className="grid grid-cols-6 gap-2 border-b border-input last:border-b-0 "
+              >
                 {r.building_name ? (
-                  <div className="col-span-3 px-2 py-2">
-                    <div className="font-medium">{r.site_name}</div>
-                    <div>{r.building_name}</div>
+                  <div className="col-span-3 px-2 py-2 flex flex-col gap-1">
+                    <div className="font-medium text-sm xl:text-base">
+                      {r.site_name}
+                    </div>
+                    <div className="text-xs xl:text-sm">{r.building_name}</div>
                   </div>
                 ) : (
-                  <div className="col-span-3 px-2 py-2 font-medium">{r.site_name}</div>
+                  <div className="col-span-3 px-2 py-2 font-medium">
+                    {r.site_name}
+                  </div>
                 )}
                 <div className="col-span-1 px-2 py-2">{r.levels}</div>
                 <div className="col-span-1 px-2 py-2">{r.chutes}</div>
-                <div className="col-span-1 text-right px-2 py-2">{formatMoney(getNumber(r.price))}</div>
+                <div className="col-span-1 text-right px-2 py-2">
+                  {formatMoney(getNumber(r.price))}
+                </div>
               </div>
             ))}
           </div>
         </div>
-
-        {/* Mobile */}
-        <div className="sm:hidden w-full flex flex-col rounded-xl bg-neutral-75 p-4">
+         {/* Mobile */}
+         <div className="sm:hidden w-full flex flex-col rounded-xl bg-neutral-75 px-2 py-4">
           <div className="grid grid-cols-2 gap-2 border-b border-input text-sm xl:text-base">
             <div className="col-span-1 px-2 py-2 text-xs">Services</div>
-            <div className="col-span-1 text-right px-2 py-2 text-xs">Price</div>
+            <div className="col-span-1 text-right px-2 py-2 text-xs">
+              Price per chute <br></br> (excl. GST)
+            </div>
           </div>
           {details.items.map((r, i) => (
-            <div key={`${r.site_id}-${r.building_id}-${i}`} className="flex flex-row gap-2 border-b border-input last:border-b-0 text-sm xl:text-base">
-              <div className="w-full px-2 py-2">
-                <div className="flex flex-col gap-1">
-                  <div className="font-medium">{r.site_name}</div>
-                  {r.building_name && <div className="text-xs">{r.building_name}</div>}
+            <div
+              key={i}
+              className="flex flex-col border-b border-input last:border-b-0 py-2 px-2"
+            >
+              <div className="font-medium text-sm">
+                {r.site_name} {r.building_name && `- ${r.building_name}`}
+              </div>
+              <div className="flex flex-row gap-2 items-center justify-between text-sm pl-1">
+          
+                  <div className="flex flex-col gap-1">
+                    <div className="flex flex-row gap-2 items-center">
+                      <div className="">{r.levels} × Levels</div>
+                      <div className="">{r.chutes} × Chutes</div>
+                    </div>
+                  </div>
+                
+                <div className="text-right px-2 py-2 w-fit flex-shrink-0">
+                  {formatMoney(getNumber(r.price))}
                 </div>
               </div>
-              <div className="text-right px-2 py-2 w-fit flex-shrink-0">{formatMoney(getNumber(r.price))}</div>
             </div>
           ))}
         </div>
